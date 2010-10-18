e512594b48065c337af9035dc8fb34fa14cd08ae
Philippe Bruhat (BooK) <book@cpan.org>
2010-10-18 23:18:39 +0200

    t/info.pl: also dump %ENV

--- t/info.pl
+++ t/info.pl
@@ -3,5 +3,12 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-print Data::Dumper->Dump( [ { name => $0, pid => $$, argv => \@ARGV } ],
-    ['info'] );
+print Data::Dumper->Dump(
+    [   {   name => $0,
+            pid  => $$,
+            argv => \@ARGV,
+            env  => \%ENV,
+        }
+    ],
+    ['info']
+);
