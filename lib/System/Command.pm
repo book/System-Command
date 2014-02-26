d4083f2086ab7abd263c28c2ae1890e967894d25
Philippe Bruhat (BooK) <book@cpan.org>
2014-02-26 21:59:34 +0100

    only trace failure if trace is requested

--- lib/System/Command.pm
+++ lib/System/Command.pm
@@ -218,7 +218,7 @@ sub new {
 
     # FIXME - better check error conditions
     if ( !defined $pid ) {
-        print $th "System::Command: ! - @cmd\n";
+        print $th "System::Command: ! - @cmd\n" if $trace;
         croak $@;
     }
 
